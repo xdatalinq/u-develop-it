75a1acdc792ad7e59db19f3d0ed02209afb15a89
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-16 21:26:04 -0400

    created votes table

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
-// Get all candidates
-router.get('/api/candidates', (req, res) => {
+// Get all candidates and their party affiliation
+router.get('/candidates', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
-             AS party_name 
-             FROM candidates 
-             LEFT JOIN parties 
-             ON candidates.party_id = parties.id`;
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -23,14 +23,14 @@ router.get('/api/candidates', (req, res) => {
   });
 });
 
-// Get a single candidate
-router.get('/api/candidate/:id', (req, res) => {
+// Get single candidate with party affiliation
+router.get('/candidate/:id', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
-             AS party_name 
-             FROM candidates 
-             LEFT JOIN parties 
-             ON candidates.party_id = parties.id 
-             WHERE candidates.id = ?`;
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -45,14 +45,76 @@ router.get('/api/candidate/:id', (req, res) => {
   });
 });
 
+// Create a candidate
+router.post('/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected, party_id) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.industry_connected,
+    body.party_id
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Update a candidate's party
+router.put('/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
 // Delete a candidate
-router.delete('/api/candidate/:id', (req, res) => {
+router.delete('/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, result) => {
     if (err) {
-      res.statusMessage(400).json({ error: res.message });
+      res.status(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
         message: 'Candidate not found'
@@ -62,59 +124,6 @@ router.delete('/api/candidate/:id', (req, res) => {
         message: 'deleted',
         changes: result.affectedRows,
         id: req.params.id
-      });
-    }
-  });
-});
-
-// Create candidate
-router.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-      VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });    
-});
-
-// Update a candidate's party
-router.put('/api/candidate/:id', (req, res) => {
-    const errors = inputCheck(req.body, 'party_id');
-
-    if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-    }
-      
-    const sql = `UPDATE candidates SET party_id = ? 
-               WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-        res.status(400).json({ error: err.message });
-        // check if a record was found
-        } else if (!result.affectedRows) {
-        res.json({
-            message: 'Candidate not found'
-        });
-        } else {
-        res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows
       });
     }
   });
--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// Get all voters alphabetized by last name
 router.get('/voters', (req, res) => {
   const sql = `SELECT * FROM voters ORDER BY last_name`;
 
@@ -13,7 +14,7 @@ router.get('/voters', (req, res) => {
     }
     res.json({
       message: 'success',
-      data: rows,
+      data: rows
     });
   });
 });
@@ -35,14 +36,15 @@ router.get('/voter/:id', (req, res) => {
   });
 });
 
-// POST route
+// Create a voter
 router.post('/voter', ({ body }, res) => {
-    // Data validation
-    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
-    if (errors) {
+  // Data validation
+  const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+  if (errors) {
     res.status(400).json({ error: errors });
     return;
-}  
+  }
+
   const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.email];
 
@@ -58,6 +60,7 @@ router.post('/voter', ({ body }, res) => {
   });
 });
 
+// Update a voter's email
 router.put('/voter/:id', (req, res) => {
   // Data validation
   const errors = inputCheck(req.body, 'email');
@@ -86,6 +89,7 @@ router.put('/voter/:id', (req, res) => {
   });
 });
 
+// Delete a voter
 router.delete('/voter/:id', (req, res) => {
   const sql = `DELETE FROM voters WHERE id = ?`;
 
